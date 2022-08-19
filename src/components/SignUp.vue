335491dfeffe7b74835a9440454a0970c11e1e2d
m-gonca <monicagonca1992@gmail.com>
2022-08-19 09:20:12 +0200

    sign in and sign up

---
+++ src/components/SignUp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Sign Up</div>
+  <PersonalRouter :route="route" :buttonText="buttonText" />
+  <p>Good Music, Patience and a lot effort</p>
+  <p>Keep calm and code on!</p>
+</template>
+
+<script setup>
+import PersonalRouter from "./PersonalRouter.vue";
+
+// Route Variables
+const route = "/auth/login";
+const buttonText = "Test the Sign In Route";
+
+// Input Fields
+
+// Error Message
+
+// Show hide password variable
+
+// Show hide confrimPassword variable
+
+// Router to push user once SignedUp to Log In
+
+// Arrow function to SignUp user to supaBase with a timeOut() method for showing the error
+</script>
+
+<style></style>
